c40f82da79c35d97bdb9a9a7ae80d0a71c006ca9
Stanley Goodwin <github@staniel.dev>
2024-05-31 13:29:04 -0400

    Update Syncopation.java

--- src/main/java/com/stanieldev/syncopation/Syncopation.java
+++ src/main/java/com/stanieldev/syncopation/Syncopation.java
@@ -1,6 +1,7 @@
 package com.stanieldev.syncopation;
 
 import com.mojang.logging.LogUtils;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.food.FoodProperties;
@@ -27,9 +28,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
+
 @Mod(Syncopation.MOD_ID)
 public class Syncopation
 {
